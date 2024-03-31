3f4eb72081eca8362d76cea7ab1b7febd334a9e8
TwelveStein <ilyaprudenko@mail.ru>
2024-03-31 19:55:27 +0300

    Version 2.0 CData update

--- RealwebQRDecrypter.py
+++ RealwebQRDecrypter.py
@@ -9,17 +9,6 @@ with open('token.txt', 'r') as file:
     TOKEN = file.read().strip()
 
 bot = telebot.TeleBot(TOKEN)
-@bot.message_handler(content_types=['photo'])
-def handle_docs_photo(message):
-    success = False  # Инициализируем флаг успеха
-    try:
-        success = process_image(message)  # Если функция вернула True, значит, операция была успешной
-    except Exception as e:
-        bot.reply_to(message, "Произошла системная ошибка, попробуйте снова")  # Если что-то пошло не так, отправляем сообщение об ошибке
-    finally:
-        if not success:  # Если операция не была успешной, отправляем сообщение о неудачной расшифровке
-            #bot.reply_to(message, "Не удалось обработать изображение!")
-            bot.reply_to(message, "Ошибка: не могу прочесть штрих код(\n\nПопробуйте выделить скриншотом область штрих-кода на фотографии и повторить попытку")
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -86,59 +75,74 @@ def send_barcode_info(message):
     bot.send_message(message.chat.id, barcode_info_text)
 
 
+@bot.message_handler(content_types=['photo'])
+def handle_docs_photo(message):
+    try:
+        process_image(message)
+    except Exception as e:
+        bot.reply_to(message, "Произошла системная ошибка, попробуйте снова")
+
 def process_image(message):
-    file_info = bot.get_file(message.photo[-1].file_id)  # Получаем информацию о файле
+    file_info = bot.get_file(message.photo[-1].file_id)
     url='https://api.telegram.org/file/bot{0}/{1}'
-    max_retries = 3  # Максимальное количество попыток
-    retry_count = 0
+    file = requests.get(url.format(TOKEN, file_info.file_path), timeout=120)
 
-    while retry_count < max_retries:
-        try:
-            # Ваша команда или операция, которая может вызвать ошибку
-            file = requests.get(url.format(TOKEN, file_info.file_path), timeout=120)
-            print("Операция выполнена успешно.")
-            break  # Выход из цикла при успешном выполнении
-        except requests.exceptions.ReadTimeout:
-            print("Произошла ошибка ReadTimeout, повторяем операцию.")
-            retry_count += 1
-    else:
-        print(f"Достигнуто максимальное количество попыток ({max_retries}).")
+    def process_zte(data):
+        return data.replace("ztxg", "5a545847")
+
+    def process_huawei(data):
+        return data
+
+    def process_cdata(data):
+        mac_address = None
+        p_sn = None
+        if "70a5" in data:  # Предполагаем, что "70a5" присутствует в MAC
+            mac_address = data
+        if "hwtc" in data:  # Предполагаем, что "hwtc" присутствует в P-SN
+            p_sn = data.replace("hwtc", "48575443")
+        return mac_address, p_sn
 
 
-    img = Image.open(BytesIO(file.content))  # Открываем изображение
-    barcodes = decode(img)  # Распознаем штрих-коды
-    vendorONU = "Неизвестное ONU устройство"
-    for barcode in barcodes:
+
+    def process_stels(data):
+        return data
+
+    vendor_codes = {
+        "ztxg": ("ZTE", process_zte),
+        "hwtc": ("CData", process_cdata),
+        "e067": ("STELS", process_stels),
+        "48575443": ("Huawei", process_huawei)
+    }
+
+    img = Image.open(BytesIO(file.content))
+    barcodes = decode(img)
+
+    mac_address = None
+    p_sn = None
+    vendor = None
+    for barcode in sorted(barcodes, key=lambda b: "70a5" in b.data.decode('utf-8').lower(), reverse=True):
         decoded_data = barcode.data.decode('utf-8').lower()
-        if "ztxg" in decoded_data:
-            vendorONU = "ZTE"
-        elif "hwtc" in decoded_data:
-            vendorONU = "CData"
-        elif "e067" in decoded_data:
-            vendorONU = "STELS"
-        elif "48575443" in decoded_data:
-            vendorONU = "Huawei"
-        if vendorONU == "Неизвестное ONU устройство":
-            #bot.reply_to(message, "ERR: не могу прочесть штрих код(\n\nПопробуйте выделить скриншотом область штрих-кода на фотографии и повторить попытку")
-            data = "\n".join([f"Найден штрих-код <code>{barcode.data.decode('utf-8').lower()}</code>" for barcode in barcodes])
-            # Отправляем информацию о всех штрих-кодах пользователю в одном сообщении
-            bot.reply_to(message, f"{vendorONU}\n\n{data}\n\nПароль: <code>IPoE-Opt82</code>", parse_mode='HTML')
-            return True  # Возвращаем True, так как операция была успешной
-        elif "70a5" not in decoded_data or "ztxg" in decoded_data or "hwtc" in decoded_data or "48575443" in decoded_data:
-            # Заменяем определенные словосочетания вендора и отправляем только отредактированную строку
-            edited_data = decoded_data.replace("hwtc", "48575443").replace("ztxg", "5a545847")
-            bot.reply_to(message, f"Вендор: <b>{vendorONU}</b>\n\nЛогин: <code>{edited_data}</code>\nПароль: <code>IPoE-Opt82</code>", parse_mode='HTML')
-            return True  # Возвращаем True, так как операция была успешной
-        elif "70a5" in decoded_data:
-            bot.reply_to(message, f"Вендор: <b>ZTE</b>\n\n</b>\n\nЛогин(MAC): <code>{decoded_data}</code>\nПароль: <code>IPoE-Opt82</code>", parse_mode='HTML')
-            return True  # Возвращаем True, так как операция была успешной
-        elif "e067" in decoded_data:
-            bot.reply_to(message, f"Вендор: <b>STELS</b>\n\n</b>\n\nЛогин(MAC): <code>{decoded_data}</code>\nПароль: <code>IPoE-Opt82</code>", parse_mode='HTML')
-            return True  # Возвращаем True, так как операция была успешной
-        else:
-            data = "\n".join([f"Найден штрих-код <code>{barcode.data.decode('utf-8').lower()}</code>" for barcode in barcodes])
-            # Отправляем информацию о всех штрих-кодах пользователю в одном сообщении
-            bot.reply_to(message, f"{data}\n\nПароль: <code>IPoE-Opt82</code>", parse_mode='HTML')
-            return True  # Возвращаем True, так как операция была успешной
+        for code, (v, processor) in vendor_codes.items():
+            if code in decoded_data:
+                edited_data = processor(decoded_data)
+                if v == "CData":
+                    mac, psn = edited_data
+                    if mac is not None:
+                        mac_address = mac
+                    if psn is not None:
+                        p_sn = psn
+                    vendor = v
+                else:
+                    vendor = v
+                    login = edited_data
+
+    if vendor == "CData":
+        bot.reply_to(message, f"Вендор: <b>{vendor}</b>\n\nMAC адресс: <code>{mac_address}</code>\nP-SN: <code>{p_sn}</code>\nПароль: <code>IPoE-Opt82</code>", parse_mode='HTML')
+    elif vendor is not None:
+        bot.reply_to(message, f"Вендор: <b>{vendor}</b>\n\nЛогин: <code>{login}</code>\nПароль: <code>IPoE-Opt82</code>", parse_mode='HTML')
+    else:
+        data = "\n".join([f"Найден штрих-код <code>{barcode.data.decode('utf-8').lower()}</code>" for barcode in barcodes])
+        bot.reply_to(message, f"Неизвестное ONU устройство\n\n{data}\n\nПароль: <code>IPoE-Opt82</code>", parse_mode='HTML')
+
 
 bot.polling(none_stop=True, interval=5)
